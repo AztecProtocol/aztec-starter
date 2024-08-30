0ecf785511392f01c70b593472912053f6bc9a8a
josh crites <jc@joshcrites.com>
2024-08-30 10:51:49 -0400

    add scripts

---
+++ scripts/getBlock.ts
@@ -0,0 +1,27 @@
+import { AccountWallet, CompleteAddress, ContractDeployer, createDebugLogger, Fr, PXE, waitForPXE, TxStatus, createPXEClient, getContractInstanceFromDeployParams, DebugLogger } from "@aztec/aztec.js";
+
+const setupSandbox = async () => {
+    const { PXE_URL = 'http://localhost:8080' } = process.env;
+    const pxe = createPXEClient(PXE_URL);
+    await waitForPXE(pxe);
+    return pxe;
+};
+
+async function main() {
+
+    let pxe: PXE;
+    let wallets: AccountWallet[] = [];
+    let accounts: CompleteAddress[] = [];
+    let logger: DebugLogger;
+
+    logger = createDebugLogger('aztec:aztec-starter');
+
+    pxe = await setupSandbox();
+
+    let block = await pxe.getBlock(1);
+    console.log(block)
+    console.log(block?.hash())
+
+}
+
+main();
