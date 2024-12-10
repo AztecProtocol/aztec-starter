12d3ed1d5c537c4843aab906aea3a8912180b7ac
josh crites <jc@joshcrites.com>
2024-12-10 13:50:58 -0500

    working

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,6 +4,7 @@ import { getSchnorrAccount } from '@aztec/accounts/schnorr';
 import { AztecAddress, deriveSigningKey } from '@aztec/circuits.js';
 import { TokenContract } from "@aztec/noir-contracts.js";
 import { getInitialTestAccountsWallets } from "@aztec/accounts/testing";
+import { SimpleLoggingContract } from "../src/artifacts/SimpleLogging.js";
 
 const setupSandbox = async () => {
     const { PXE_URL = 'http://localhost:8080' } = process.env;
@@ -23,17 +24,19 @@ async function main() {
 
     pxe = await setupSandbox();
     wallets = await getInitialTestAccountsWallets(pxe);
+    let contract = await SimpleLoggingContract.deploy(wallets[0]).send().deployed()
+    logger.info(`Contract deployed: ${contract.address}`)
+    // let secretKey = Fr.random();
+    // let salt = Fr.random();
 
-    let secretKey = Fr.random();
-    let salt = Fr.random();
+    // let schnorrAccount = await getSchnorrAccount(pxe, secretKey, deriveSigningKey(secretKey), salt);
+    // const { address, publicKeys, partialAddress } = schnorrAccount.getCompleteAddress();
+    // let tx = await schnorrAccount.deploy().wait();
+    // console.log("tx", tx);
+    // let wallet = await schnorrAccount.getWallet();
 
-    let schnorrAccount = await getSchnorrAccount(pxe, secretKey, deriveSigningKey(secretKey), salt);
-    const { address, publicKeys, partialAddress } = schnorrAccount.getCompleteAddress();
-    let tx = await schnorrAccount.deploy().wait();
-    let wallet = await schnorrAccount.getWallet();
-    
-    const votingContract = await EasyPrivateVotingContract.deploy(wallet, address).send().deployed();
-    logger.info(`Voting Contract deployed at: ${votingContract.address}`);
+    // const votingContract = await EasyPrivateVotingContract.deploy(wallet, address).send().deployed();
+    // logger.info(`Voting Contract deployed at: ${votingContract.address}`);
 }
 
 main();
