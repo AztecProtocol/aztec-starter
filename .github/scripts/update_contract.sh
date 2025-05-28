795e71aa16017ee563cd3838bef517432c4edad0
josh crites <jc@joshcrites.com>
2025-05-28 12:23:17 -0400

    Update update_contract.sh

--- .github/scripts/update_contract.sh
+++ .github/scripts/update_contract.sh
@@ -7,7 +7,7 @@ echo "version tag: $version_tag"
 nargo_file_path="$copy_to_file_path/Nargo.toml"
 
 repo_url="https://github.com/AztecProtocol/aztec-packages.git"
-contracts_path="noir-projects/noir-contracts/contracts"
+contracts_path="noir-projects/noir-contracts/contracts/app"
 
 # Check if the file exists
 if [ ! -f "$nargo_file_path" ]; then
